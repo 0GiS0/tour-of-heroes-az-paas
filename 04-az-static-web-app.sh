b5e9f22a9b273143722faaaad30eb57211b3c9f8
0gis0 <giselatb@outlook.com>
2023-12-30 17:47:16 +0000

    Update Azure Static Web App configuration

--- 04-az-static-web-app.sh
+++ 04-az-static-web-app.sh
@@ -1,12 +1,13 @@
 echo -e "Create Azure Static Web App 🚀"
 
 az staticwebapp create \
---name tour-of-heroes-web \
+--name $WEB_APP_NAME \
 --resource-group $RESOURCE_GROUP \
 --source https://github.com/$GITHUB_USER_NAME/tour-of-heroes-angular \
---location $LOCATION \
+--location "westeurope" \
 --branch main \
---app-location "src" \
+--app-location "/" \
+--output-location "dist/angular-tour-of-heroes" \
 --login-with-github
 
 # https://azure.github.io/static-web-apps-cli/
